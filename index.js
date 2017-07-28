e00aba7f1219cbf545d3f52f8831e897adc550ab
Aaron Contreras <aaroncontreras1990@gmail.com>
2017-07-28 18:58:58 -0400

    first push

---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var restify = require('restify');
+var jwtr = require('restify-jwt');
+var jwt = require('jsonwebtoken');
+var server = restify.createServer({
+  name: 'myapp',
+  version: '1.0.0'
+})
+
+var db_config = {
+  host: "69.87.220.221",
+  user: "admin_pehamr",
+  password: "Jimmy.ascacc31130724",
+  database: "admin_tax_pupilo"
+};
+
+var secretToken = '1d=5YA@t_d/+_E%h';
+
+server.use(restify.plugins.bodyParser());
+
+server.use(function (req,res,next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    return next();
+});
+
+server.use(function (req, res, next){
+    if(req.url == '/user/create/' ||  req.url == '/user/login/'){
+        return next();
+    }
+
+    // check header or url parameters or post parameters for token
+    var token = req.headers['authorization'];
+    // decode token
+    if (token) {
+      // verifies secret and checks exp
+      jwt.verify(token, secretToken, function(err, decoded) {      
+        if (err) {
+          return res.json({ success: false, message: err });    
+        } else {
+          // if everything is good, save to request for use in other routes
+          req.decoded = decoded;  
+          return next();
+        }
+      });
+    } else {
+      //avoid create user and login
+
+ 
+      // if there is no token
+      // return an error
+      return res.send(500,{ 
+          success: false, 
+          message: 'No token provided.' 
+      });
+
+    }
+});
+
+
+
+  
+
+require('./user')(server,db_config,secretToken);
+require('./formW2')(server,db_config);
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
+
---
+++ user.js
@@ -0,0 +1,217 @@
+'use strict';
+
+var mysql = require('mysql');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
+module.exports = function(server, db_config, secret){
+
+  var connection; 
+  var table = 'tx_user';
+
+  server.get('/user/', (req, res, next) => {
+    
+    conectionDB(); 
+
+    connection.query("SELECT * FROM " + table , function (err, result, fields) {
+    
+      if (err) throw err;
+      
+      res.send(200,result);
+
+      connection.end();
+
+      return next(false);
+     
+    });
+    
+    return next();
+    
+  });
+
+  server.get('/user/:id', (req, res, next) => {
+
+    conectionDB(); 
+
+    connection.query("SELECT * FROM " + table + " Where userId = ?" , [req.params.id], function (err, result, fields) {
+    
+      if (err) throw err;
+
+      var user = result[0];
+      
+      res.send(200, user);
+
+      connection.end();
+
+      return next(false);
+    });
+
+    return next();
+
+  });
+
+  server.post('/user/create/', (req, res, next) => {
+    
+    if( req.body.hasOwnProperty('email') && req.body.hasOwnProperty('password') && req.body.hasOwnProperty('password_confirm')){
+      if(req.body.email != "" && req.body.password != "" &&  req.body.password_confirm != ""){
+        if(req.body.password == req.body.password_confirm){
+
+          var query = "INSERT INTO " + table + " (`email`,`password`,`salt`) VALUES (?,?,?);";
+
+          var salt = genRandomString(16); /** Gives us salt of length 16 */
+          var passwordData = sha512(req.body.password, salt);
+
+          console.log(passwordData.passwordHash);
+
+          conectionDB();
+
+          connection.query(query , [req.body.email, passwordData.passwordHash, passwordData.salt], function (err, result, fields) {
+    
+            if (err){
+              res.send(500, {message: err});
+              connection.end();
+              return next(false);
+            }
+
+            var user = result[0];
+            
+            res.send(200, {message:"Inserted successfully"});
+
+            connection.end();
+
+            return next(false);
+          });
+
+        }else{
+          //match the passwords
+          res.send(500, {message:"Passwords are differents"});
+        }
+      }else{
+        //data cant be null
+        res.send(500, {message:"Data can't be null"});
+      }
+    }else{
+      //missin parameter
+      res.send(500, {message:"Missing parameters"});
+    }
+
+    return next(false);
+  });  
+
+  server.post('/user/login/', (req, res, next) => {
+    
+    if( req.body.hasOwnProperty('email') && req.body.hasOwnProperty('password')){
+      
+      if(req.body.email != "" && req.body.password != "" &&  req.body.password_confirm != ""){
+
+        var query = "Select * From " + table + " WHERE email = ?";
+
+        conectionDB();
+
+        connection.query(query , [req.body.email], function (err, result, fields) {
+    
+          if (err){
+            res.send(500, {message: err});
+            connection.end();
+            return next(false);
+          }
+
+          if(result != undefined && result[0] != undefined){
+            var user = result[0];
+
+            var passwordData = sha512(req.body.password, user.salt);
+
+            if(passwordData.passwordHash == user.password){
+
+              var token = jwt.sign(user, secret , {
+                expiresIn: 1440 // expires in 24 hours
+              });
+
+              // return the information including token as JSON
+              res.json({
+                success: true,
+                message: 'Enjoy your token!',
+                token: token
+              });
+
+            }else{
+
+              //missin parameter
+              res.send(500, {message:"Password Invalid"});
+
+            }
+          }else{
+            //missin parameter
+            res.send(500, {message:"User Doesn't Exist"});
+            
+          }
+          
+          connection.end();
+
+          return next(false);
+        });
+
+      }else{
+        //data cant be null
+        res.send(500, {message:"Data can't be null"});
+      }
+    }else{
+      //missin parameter
+      res.send(500, {message:"Missing parameters"});
+    }
+
+    return next(false);
+  });
+
+
+  function conectionDB(){
+      
+    connection = mysql.createConnection(db_config);
+    connection.connect(function(err) {              
+      if(err) {                                     
+        console.log('error when connecting to db:', err);
+        throw err; 
+      }                                     
+    }); 
+
+    connection.on('error', function(err) {
+      console.log('db error', err.code);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+        conectionDB();                         
+      } else {                                      
+        throw err;                                  
+      }
+    });                               
+  }
+  /**
+   * generates random string of characters i.e salt
+   * @function
+   * @param {number} length - Length of the random string.
+   */
+  var genRandomString = function(length){
+      return crypto.randomBytes(Math.ceil(length/2))
+              .toString('hex') /** convert to hexadecimal format */
+              .slice(0,length);   /** return required number of characters */
+  };
+
+  /**
+   * hash password with sha512.
+   * @function
+   * @param {string} password - List of required fields.
+   * @param {string} salt - Data to be validated.
+   */
+  var sha512 = function(password, salt){
+      var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
+      hash.update(password);
+      var value = hash.digest('hex');
+      return {
+          salt:salt,
+          passwordHash:value
+      };
+  };
+
+
+
+
+};
+
